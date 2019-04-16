f461df0cb4190f37612d6f84ef97729956e133ab
Valery Kalugin <kalugin.1912@gmail.com>
2019-04-16 23:39:47 +0300

    Touch Helper Callback

---
+++ app/src/main/java/com/kalugin/draggablegridview/ItemAdapter.java
@@ -0,0 +1,76 @@
+package com.kalugin.draggablegridview;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.RandomAccess;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.Holder> {
+
+    private List<String> mItems = new ArrayList<>();
+
+    public void init(){
+        for (int i = 0; i<400; i++){
+            mItems.add(i  + "AAAAA");
+        }
+    }
+
+
+    public ItemAdapter() {
+        init();
+    }
+
+    @NonNull
+    @Override
+    public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new Holder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Holder holder, int position) {
+        holder.textView.setText(mItems.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItems.size();
+    }
+
+    public void onItemDismiss(int position) {
+        mItems.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public boolean onItemMove(int fromPosition, int toPosition) {
+        if (fromPosition < toPosition) {
+            for (int i = fromPosition; i < toPosition; i++) {
+                Collections.swap(mItems, i, i + 1);
+            }
+        } else {
+            for (int i = fromPosition; i > toPosition; i--) {
+                Collections.swap(mItems, i, i - 1);
+            }
+        }
+        notifyItemMoved(fromPosition, toPosition);
+        return true;
+    }
+
+    class Holder extends RecyclerView.ViewHolder{
+
+        private TextView textView;
+
+        public Holder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.item);
+        }
+    }
+}
--- app/src/main/java/com/kalugin/draggablegridview/MainActivity.java
+++ app/src/main/java/com/kalugin/draggablegridview/MainActivity.java
@@ -1,7 +1,11 @@
 package com.kalugin.draggablegridview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView recyclerView = findViewById(R.id.recycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 4, RecyclerView.VERTICAL, false));
+        ItemAdapter itemAdapter = new ItemAdapter();
+        recyclerView.setAdapter(itemAdapter);
+        ItemTouchHelper.Callback callback =
+                new ItemTouchHelperCallback(itemAdapter);
+        ItemTouchHelper touchHelper = new ItemTouchHelper(callback);
+        touchHelper.attachToRecyclerView(recyclerView);
     }
 }
